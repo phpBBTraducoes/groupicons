a5ef7eafa737f2dab87f6899771ccb12f32f5145
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-01-08 13:13:45 -0200

    ADD

---
+++ language/pt_br/common.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* This file is part of the Group Icons package.
+* Brazilian Portuguese translation by eunaumtenhoid (c) 2019 [ver 0.2.0] (https://github.com/phpBBTraducoes)
+* @copyright (c) VinaBB <vinabb.vn>
+* @license The MIT License (MIT)
+*/
+
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+$lang = array_merge($lang, array(
+	'GROUP_ICON'		=> 'Ícone do grupo',
+	'SELECT_GROUP_ICON'	=> 'Selecione um ícone do grupo…',
+));
